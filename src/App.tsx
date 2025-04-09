204fe394c440345aa331749c01b77389a3abd6ca
Kazte <castellittofranco1@gmail.com>
2025-04-08 22:36:02 -0300

    feat: integrate error handling in router context

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 
+import ErrorComponent from "./components/ui/error-component";
 import { Loader2Icon } from "lucide-react";
-import useAuth from "./hooks/use-auth";
 import { routeTree } from "./routeTree.gen";
+import useAuth from "./hooks/use-auth";
 
 const router = createRouter({
   routeTree,
   context: { authentication: undefined! },
+  defaultErrorComponent: (e) => <ErrorComponent error={e} />,
 });
 
 declare module "@tanstack/react-router" {
---
+++ src/components/ui/error-component.tsx
@@ -0,0 +1,66 @@
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "./accordion";
+import { Alert, AlertDescription, AlertTitle } from "./alert";
+import { ErrorComponentProps, Link, useRouter } from "@tanstack/react-router";
+
+import { AlertTriangleIcon } from "lucide-react";
+import { Button } from "./button";
+
+export default function ErrorComponent({
+  error,
+}: { error: ErrorComponentProps }) {
+  const router = useRouter();
+  const isDev = process.env.NODE_ENV === "development";
+
+  return (
+    <div className="flex size-full flex-col items-center justify-center self-center p-2 text-2xl flex-grow">
+      <div className="w-full max-w-md">
+        <Alert variant="destructive">
+          <AlertTriangleIcon className="size-4" />
+          <AlertTitle>Ooops! Something went wrong</AlertTitle>
+          <AlertDescription>
+            We&apos;re sorry, but we encoutered an unexpected error.
+          </AlertDescription>
+        </Alert>
+
+        <div className="mt-4 space-y-4">
+          <Button
+            className="w-full"
+            onClick={() => {
+              router.invalidate();
+            }}
+          >
+            Try again
+          </Button>
+          <Button className="w-full" variant="outline" asChild>
+            <Link to="/">Return to Home</Link>
+          </Button>
+        </div>
+      </div>
+      {isDev ? (
+        <div className="mt-4 w-full max-w-5xl">
+          <Accordion type="single" className="w-full" collapsible>
+            <AccordionItem value="error-details">
+              <AccordionTrigger>View Error details</AccordionTrigger>
+              <AccordionContent>
+                <div className="relative rounded-md bg-muted p-4">
+                  <h3 className="mb-2 font-semibold">Error message:</h3>
+                  <p className="mb-4 text-sm">{error.error.message}</p>
+
+                  <h3 className="mb-2 font-semibold">Stack trace:</h3>
+                  <pre className="whitespace-pre-wrap text-sm">
+                    {error.error.stack}
+                  </pre>
+                </div>
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
+        </div>
+      ) : null}
+    </div>
+  );
+}
