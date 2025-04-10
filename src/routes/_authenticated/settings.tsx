4f0a90c7d55404400161aa35f064dd80b7c5d70d
Kazte <castellittofranco1@gmail.com>
2025-04-10 00:47:53 -0300

    feat: refactor settings tabs

--- src/routes/_authenticated/settings.tsx
+++ src/routes/_authenticated/settings.tsx
@@ -1,6 +1,7 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
-import OverviewTab from "@/components/tabs/settings/overview-tab";
+import DangerTab from "@/components/tabs/settings/danger-tab";
+import ProfileTab from "@/components/tabs/settings/profile-tab";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/_authenticated/settings")({
@@ -9,35 +10,36 @@ export const Route = createFileRoute("/_authenticated/settings")({
 
 function RouteComponent() {
   return (
-    <Tabs defaultValue="overview" className="flex flex-col gap-2.5 w-full">
-      <TabsList className="">
-        <TabsTrigger className="w-full" value="overview">
-          Overview
-        </TabsTrigger>
-        <TabsTrigger className="w-full" value="profile">
-          Profile
-        </TabsTrigger>
-        <TabsTrigger className="w-full" value="connections">
+    <section className="size-full flex flex-col justify-center space-y-4">
+      <h1 className="text-2xl font-bold">Settings</h1>
+      <Tabs defaultValue="profile" className="flex flex-col gap-2.5 w-full">
+        <TabsList className="grid w-full grid-cols-2">
+          <TabsTrigger className="w-full" value="profile">
+            Profile
+          </TabsTrigger>
+          {/* <TabsTrigger className="w-full" value="connections">
           Connections
         </TabsTrigger>
         <TabsTrigger className="w-full" value="sessions">
           Sessions
-        </TabsTrigger>
-        <TabsTrigger className="w-full" value="danger">
-          Danger Zone
-        </TabsTrigger>
-      </TabsList>
-      <TabsContent value="overview">
-        <OverviewTab />
-      </TabsContent>
+        </TabsTrigger> */}
+          <TabsTrigger className="w-full" value="danger">
+            Danger Zone
+          </TabsTrigger>
+        </TabsList>
 
-      <TabsContent value="profile"></TabsContent>
+        <TabsContent value="profile">
+          <ProfileTab />
+        </TabsContent>
 
-      <TabsContent value="connections"></TabsContent>
+        {/* <TabsContent value="connections"></TabsContent>
 
-      <TabsContent value="sessions"></TabsContent>
+      <TabsContent value="sessions"></TabsContent> */}
 
-      <TabsContent value="danger"></TabsContent>
-    </Tabs>
+        <TabsContent value="danger">
+          <DangerTab />
+        </TabsContent>
+      </Tabs>
+    </section>
   );
 }
