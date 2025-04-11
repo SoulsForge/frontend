1c6abded35171e446f7812b5f411e0e5066119ea
Kazte <castellittofranco1@gmail.com>
2025-04-11 18:33:26 -0300

    feat: enhance error handling in ErrorComponent for offline and server errors

--- src/components/layout/error-component.tsx
+++ src/components/layout/error-component.tsx
@@ -16,6 +16,47 @@ export default function ErrorComponent({
   const router = useRouter();
   const isDev = process.env.NODE_ENV === "development";
 
+  const networkError = error.error.message.includes("NetworkError");
+  const isOffline = navigator.onLine === false;
+
+  const isOfflineError = networkError && isOffline;
+  const isServerError = networkError && !isOffline;
+
+  if (isOfflineError) {
+    return (
+      <div className="flex size-full flex-col items-center justify-center self-center p-2 text-2xl flex-grow">
+        <div className="w-full max-w-md">
+          <Alert variant="destructive">
+            <AlertTriangleIcon className="size-4" />
+            <AlertTitle>Ooops! You are offline</AlertTitle>
+            <AlertDescription>
+              Please check your internet connection and try again.
+            </AlertDescription>
+          </Alert>
+        </div>
+      </div>
+    );
+  }
+
+  if (isServerError) {
+    return (
+      <div className="flex size-full flex-col items-center justify-center self-center p-2 text-2xl flex-grow">
+        <div className="w-full max-w-md">
+          <Alert variant="destructive">
+            <AlertTriangleIcon className="size-4" />
+            <AlertTitle>Ooops! Server error</AlertTitle>
+            <AlertDescription>
+              We&apos;re sorry, but we encoutered an unexpected error.
+              <br />
+              Please try again later.
+            </AlertDescription>
+          </Alert>
+        </div>
+      </div>
+    );
+  }
+  
+
   return (
     <div className="flex size-full flex-col items-center justify-center self-center p-2 text-2xl flex-grow">
       <div className="w-full max-w-md">
